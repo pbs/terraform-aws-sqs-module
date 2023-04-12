36df7b18cd3cbbc2647dc2a03a546cb559e70a13
Yousif Akbar <11247449+yhakbar@users.noreply.github.com>
2023-04-12 16:29:44 -0400

    Adding descriptions to outputs

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,14 @@
 output "arn" {
-  value = aws_sqs_queue.queue.arn
+  description = "The ARN of the SQS queue"
+  value       = aws_sqs_queue.queue.arn
 }
 
 output "name" {
-  value = aws_sqs_queue.queue.name
+  description = "The name of the SQS queue"
+  value       = aws_sqs_queue.queue.name
 }
 
 output "url" {
-  value = aws_sqs_queue.queue.url
+  description = "The URL of the SQS queue"
+  value       = aws_sqs_queue.queue.url
 }
